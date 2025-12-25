41c971c86a208b91e5f86ef081e26f8d77ee2ff0
Kaustubh426344 <kaustubhtayade21@gmail.com>
2025-12-25 23:05:20 +0530

    Change S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state-rohit-426344"
+    bucket = "terraform-bucket-883033897"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
